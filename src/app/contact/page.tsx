9e5a6a8043fbac8ed7034d1fd6a066bf68cea80c
Nirav <niravlimbachia031@gmail.com>
2024-10-03 12:27:02 +0530

    completed Website

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,70 @@
+"use client";
+import { Meteors } from "@/components/ui/meteors";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import contact from "../assets/3dContact.png";
+import Image from "next/image";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div>
+      <div className="h-screen w-screen relative bg-gray-900">
+<div className="h-screen w-full flex  flex-col justify-center items-center">
 
-export default page
+<h2 className="text-4xl font-bold  underline mb-6 text-center">Contact Us</h2>
+
+        <div className="h-fit flex  gap-5  justify-center items-center">
+          <div>
+            <form className="h-fit w-full flex flex-col gap-5">
+              <div className="flex gap-5">
+                <div>
+                  <label id="name" className="sr-only">
+                    Name
+                  </label>
+                  <Input placeholder="Name" id="name" autoComplete="name" />
+                </div>
+                <div>
+                  <label id="email" className="sr-only">
+                    Email
+                  </label>
+                  <Input
+                    placeholder="Email"
+                    type="email"
+                    id="email"
+                    autoComplete="email"
+                  />
+                </div>
+              </div>
+              <div className="w-full">
+                <label id="subject" className="sr-only">
+                  Subject
+                </label>
+                <Input placeholder="Subject" id="subject" />
+              </div>
+              <div className="w-full">
+                <label id="message" className="sr-only">
+                  Message
+                </label>
+                <Textarea placeholder="Massage" id="message" rows={5}/>
+              </div>
+
+              <div className="text-center">
+                <button className="shadow-[inset_0_0_0_2px_#616467] text-gray-300 px-12 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200">
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+          <div>
+            <Image src={contact} alt="contact Image" />
+          </div>
+        </div>
+        </div>
+        {/* Meaty part - Meteor effect */}
+        <Meteors number={20} />
+      </div>
+    </div>
+  );
+};
+
+export default page;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,46 +1,29 @@
 "use client";
-import React, { useState } from "react";
-import {
-  HoveredLink,
-  Menu,
-  MenuItem,
-  ProductItem,
-} from "@/components/ui/navbar-menu";
-import { cn } from "@/lib/utils";
-import Link from "next/link";
+import React from "react";
+import { FloatingNav } from "./ui/floating-navbar";
 
 const Navbar = ({ className }: { className?: string }) => {
-  const [active, setActive] = useState<string | null>(null);
+
+  const navItems = [
+    {
+      name: "Home",
+      link: "/",
+    },
+    {
+      name: "Courses",
+      link: "/courses",
+    },
+    {
+      name: "Contact",
+      link: "/contact",
+      
+    },
+  ];
 
   return (
-    <div
-      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50 backdrop:blur-3xl ", className)}
-    >
-      <Menu setActive={setActive}>
-        <Link href={"/"}>
-          <MenuItem
-            setActive={setActive}
-            active={active}
-            item="Home"
-          ></MenuItem>
-        </Link>
-        <MenuItem setActive={setActive} active={active} item="Our Courses">
-          <div className="flex flex-col space-y-4  text-sm">
-            <HoveredLink href="/courses">All Courses </HoveredLink>
-            <HoveredLink href="/courses">Basic Music Theory </HoveredLink>
-            <HoveredLink href="/courses">Advance Composition </HoveredLink>
-            <HoveredLink href="/courses">Songwriting </HoveredLink>
-            <HoveredLink href="/courses">Music Production </HoveredLink>
-          </div>
-        </MenuItem>
-        <Link href={"/contact"}>
-          <MenuItem
-            setActive={setActive}
-            active={active}
-            item="Contact Us"
-          ></MenuItem>
-        </Link>
-      </Menu>
+    <div className="relative  w-full">
+      <FloatingNav navItems={navItems} />
+      {/* <DummyContent /> */}
     </div>
   );
 };
